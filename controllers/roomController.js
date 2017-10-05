d61e56133c521865f8c89c9289d4b89b43ad029b
Kevin Yan <k3vinyan@gmail.com>
2017-10-05 00:26:13 -0700

    room show update

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -5,20 +5,24 @@ const Room = mongoose.model('Room');
 
 exports.allRooms = async(req, res) => {
   const rooms = await Room.find();
-  console.log(typeof(rooms));
+//  console.log(rooms);
   res.render('rooms', {rooms});
 }
-exports.singleRoom = (req, res) => {
-  console.log("single room");
+
+exports.singleRoom = async(req, res) => {
+  const room = await Room.findOne( {_id: req.params.id})
+  console.log(room.name);
+  res.render('show', {room});
 }
+
 exports.newRoom = (req, res) => {
   res.render('new');
 }
+
 exports.createRoom = async (req, res) => {
   const roomName = req.body['name'];
   const room = new Room({'name': roomName});
   await room.save();
-  console.log('it worked!');
   res.redirect('/rooms');
 }
 
